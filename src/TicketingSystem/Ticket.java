f3701319dcaf2c5dcc4ff2fd161f9460edbe75ac
Tianze Shan <tshan@bu.edu>
2021-12-05 18:08:17 -0500

    Update Ticket.java

--- src/TicketingSystem/Ticket.java
+++ src/TicketingSystem/Ticket.java
@@ -1,5 +1,6 @@
 package TicketingSystem;
 import ParkingLot.Lot;
+import ParkingLot.Spot;
 
 
 
@@ -10,19 +11,22 @@ public class Ticket {
 	Instant start;
 	int ID;
 	int pricePer15Min;
+	Spot spot;
 	
 	public Ticket() {
 		this.start = Instant.now();
 		Random random = new Random();
 		this.ID = random.nextInt(999999);
 		this.pricePer15Min = 0;
+		this.spot = null;
 	}
 	
-	public Ticket(Lot lot) {
+	public Ticket(Lot lot, int type) {
 		this.start = Instant.now();
 		Random random = new Random();
 		this.ID = random.nextInt(999999);
 		this.pricePer15Min = lot.getPrice();
+		this.spot = lot.assignSpotOneTime(type);
 	}
 
 	public int getPrice(){
