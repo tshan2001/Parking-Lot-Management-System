742bf593dde7316df036731b74b4ecdef210b899
Ke Li <93945205+like101101@users.noreply.github.com>
2021-12-05 18:29:19 -0500

    Update lot.java

--- src/ParkingLot/Lot.java
+++ src/ParkingLot/Lot.java
@@ -1,14 +1,23 @@
 package ParkingLot;
+import java.util.Scanner;
+
 import Database.Database;;
 
+
 public class Lot {
+	public static int page = 0;
+	public static String main_cmds = "<Main Page>: Please enter the option number: \n	1: Sign in As Admin \n	2: Sign in As User \n	3. Parking \n	4. Register for User \n 	5. Advance Setting \n 	6. Display Parking Lot\n";
 	int MAXSIZE_Disa; /* Spots for Disability  */
 	int MAXSIZE_Comp; /* Spots for compact  */
 	int MAXSIZE_Reg;  /* Spots for regular  */
+	int num_reg;
+	int num_disa;
+	int num_comp;
 	int MAXS = MAXSIZE_Disa + MAXSIZE_Comp + MAXSIZE_Reg; /* Total Spots  */
 	int numSpots;
 	int rate; /* This parking lot uses flat rate, in dollars  */
 	Spot[] ParkingSpots; /* The array for each spot */
+	Database db;
 
 	public Lot() {
 	/* The default constructor for the parking lot*/
@@ -16,9 +25,13 @@ public class Lot {
 		this.MAXSIZE_Disa = 10;
 		this.MAXSIZE_Comp = 10;
 		this.MAXS = MAXSIZE_Disa + MAXSIZE_Comp + MAXSIZE_Reg;
-		this.numSpots = MAXS;
+		this.num_reg = MAXSIZE_Reg;
+		this.num_disa = MAXSIZE_Disa;
+		this.num_comp = MAXSIZE_Comp;
 		this.ParkingSpots = new Spot[MAXS]; /* Generate such number for spots class */
 		this.rate=10;
+		this.db = new Database();
+		
 		
 		/* Update each spot with its type */
 		for (int i=0; i<MAXSIZE_Reg;i++) {
@@ -59,6 +72,18 @@ public class Lot {
 		
 	}
 	
+	public int getReg() {
+		return this.num_reg;
+	}
+	
+	public int getComp() {
+		return this.num_comp;
+	} 
+	
+	public int getDisa() {
+		return this.num_disa;
+	}
+	
 	public int getPrice() {
 		/* Return the current rate of the Parking Lot*/
 		return this.rate;
@@ -73,17 +98,18 @@ public class Lot {
 		return this.numSpots;
 	}
 	
+	public void removeSpotReg(int SpotId) {
+	/* Given the spot, remove this Spot as a registered Spot */
+		this.ParkingSpots[SpotId].register = false;
+		this.ParkingSpots[SpotId].availabilty = true;
+	}
+	
 	public Spot assignSpotReg(int type, boolean in) {
 		/* Assign Spot to a Registered User
 		 * 0 represent regular
 		 * 1 represent disability
 		 * 2 represent compact*/
-		if (in) {
-			this.numSpots -= 1;
-			
-		}else {
-			this.numSpots += 1;
-		}
+	
 		
 		switch(type) {
 		case 0:
@@ -91,6 +117,7 @@ public class Lot {
 				if (this.ParkingSpots[i].register == false) {
 					this.ParkingSpots[i].changeReg(in);
 					this.ParkingSpots[i].availabilty = (! in);
+					this.num_reg -= 1;
 					return this.ParkingSpots[i];
 				}
 			}
@@ -99,6 +126,7 @@ public class Lot {
 				if (this.ParkingSpots[i].register == false) {
 					this.ParkingSpots[i].changeReg(in);
 					this.ParkingSpots[i].availabilty = (! in);
+					this.num_disa -= 1;
 					return this.ParkingSpots[i];
 				}
 			}
@@ -108,6 +136,7 @@ public class Lot {
 				if (this.ParkingSpots[i].register == false) {
 					this.ParkingSpots[i].changeReg(in);
 					this.ParkingSpots[i].availabilty = (! in);
+					this.num_comp -= 1;
 					return this.ParkingSpots[i];
 				}
 			}
@@ -153,24 +182,55 @@ public class Lot {
 			
 		}
 		
+		
+	}
+	
+	public void mainCmds(int main_cmd) {
+		Scanner main_scanner = new Scanner(System.in);
+		switch (main_cmd) {
+		case 1:
+			System.out.print("<Admin>: Enter Admin Username:	");
+			String username = main_scanner.nextLine();
+			System.out.print("<Admin>: Enter Admin Password:	");
+			String password = main_scanner.nextLine();
+			if (this.db.)
+			
+		case 6:
+			System.out.println("<Display>: Current Parking Lot has: ");
+			System.out.println("	Regular Spots: " + this.getReg());
+			System.out.println("	Compact Spots: " + this.getComp());
+			System.out.println("	Disability Spots: " + this.getDisa());
+		
+		}
+		main_scanner.close();
 	}
 	
 	public static void main(String args[]) {
 		Lot mylot = new Lot();
-		Database mydatabase = new Database();
 		
 		System.out.println("Default parking lot and database has generated");
+		System.out.println(" ");
 		System.out.println("The default admin Username is " + mydatabase.getSudoId());
 		System.out.println("The default admin Password is " + mydatabase.getSudoPwd());
 		System.out.println("Please use the sudo admin to gain access");
-		
-		
 		System.out.println(" ");
-		System.out.println("Now there are " + mylot.numAvail() + " free spots");
-		mylot.assignSpotReg(0, true);
-		System.out.println("Adding one spot to a register");
-		System.out.println("Now there are " + mylot.numAvail() + " free spots");
+		System.out.println(main_cmds);
 		
+		Scanner scanner = new Scanner(System.in);
+
+		while (true) {
+			if (scanner.hasNext()) {
+				switch (page) {
+				case 0:
+					int main_cmd = scanner.nextInt();
+					System.out.println(" - - - - - - - - - - - - - - ");
+					mylot.mainCmds(main_cmd);
+					System.out.println(" - - - - - - - - -  - - - - -");
+					System.out.println("");
+					System.out.println(main_cmds);
+				}
+			}
+		}
 
 	}
 
