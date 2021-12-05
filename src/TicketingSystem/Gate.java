453a25fb1ee7fea86c946615e31368712be1101d
Tianze Shan <tshan@bu.edu>
2021-12-05 17:12:26 -0500

    Update Gate.java

--- src/TicketingSystem/Gate.java
+++ src/TicketingSystem/Gate.java
@@ -1,5 +1,8 @@
 package TicketingSystem;
 
+import java.time.Duration;
+import java.time.Instant;
+
 public class Gate {
 	boolean is_open;
 	
@@ -24,5 +27,14 @@ public class Gate {
 	
 	public void cancelHold() {
 		this.is_open = false;
+	}
+
+	public static void main(String args[]){
+		Instant start = Instant.now();
+		Instant end = Instant.parse("2021-12-05T21:10:00Z");
+		System.out.println(start.toString());
+		System.out.println(end.toString());
+		Duration timeElapsed = Duration.between(start, end);
+		System.out.println("Time taken: "+ timeElapsed.toMillis() / 1000 +" milliseconds");
 	}
 }
