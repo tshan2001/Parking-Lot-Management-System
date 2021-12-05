1ea71b72b23febf186fa5d3d4745b6bd87d073ef
Tianze Shan <tshan@bu.edu>
2021-12-05 17:27:36 -0500

    Update TicketMachine.java

--- src/TicketingSystem/TicketMachine.java
+++ src/TicketingSystem/TicketMachine.java
@@ -7,6 +7,7 @@ import UserSystem.RegisteredUser;
 public class TicketMachine {
     Database database;
     String machineID;
+    Lot lot;
     int currentRate;
     int avaliableReg;
     int avaliableComp;
@@ -22,20 +23,22 @@ public class TicketMachine {
 
     public TicketMachine(String name, Lot lot){
         this.machineID = name;
+        this.lot = lot;
         this.currentRate = lot.getPrice();
         this.avaliableReg = lot.getReg();
         this.avaliableComp = lot.getComp();
         this.avaliableDisa = lot.getDisa();
     }
 
-    public void registeredEntering(String UserID, String type){
-        if (database.userExists(UserID)){
+    public boolean registeredEntering(Key userKey, String type){
+        if (database.userExists(userKey.getUserName())){
             if (type.equals("Reg")){
                 if (this.avaliableReg > 0) {
                     this.avaliableReg--;
                 }
                 else{
                     System.out.println("There's no regular parking spot available, please try other types");
+                    return false;
                 }
             }
             else if (type.equals("Disa")){
@@ -44,6 +47,7 @@ public class TicketMachine {
                 }
                 else{
                     System.out.println("There's no disability parking spot available, please try other types");
+                    return false;
                 }
             }
             else if (type.equals("Comp")){
@@ -52,12 +56,47 @@ public class TicketMachine {
                 }
                 else{
                     System.out.println("There's no compact parking spot available, please try other types");
+                    return false;
                 }
             }
-            return;
+            return true;
         }
         else{
             System.out.println("User not recogonized, please re-enter username or enter as one time visitor");
+            return false;
         }
     }
+
+    public Ticket oneTimeParking(String type) {
+        Ticket ticket = new Ticket(this.lot);
+        if (type.equals("Reg")){
+            if (this.avaliableReg > 0) {
+                this.avaliableReg--;
+                return ticket;
+            }
+            else{
+                System.out.println("There's no regular parking spot available, please try other types");
+            }
+        }
+        else if (type.equals("Disa")){
+            if (this.avaliableDisa > 0) {
+                this.avaliableDisa--;
+                return ticket;
+            }
+            else{
+                System.out.println("There's no disability parking spot available, please try other types");
+            }
+        }
+        else if (type.equals("Comp")){
+            if (this.avaliableComp > 0) {
+                this.avaliableComp--;
+                return ticket;                
+            }
+            else{
+                System.out.println("There's no compact parking spot available, please try other types");
+            }
+        }
+        return null;
+    }
+    
 }
