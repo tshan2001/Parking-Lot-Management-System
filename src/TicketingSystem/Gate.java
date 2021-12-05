db888d4bb71d704e9104d82f61dc65ed7a23fedd
Tianze Shan <tshan@bu.edu>
2021-12-05 18:27:59 -0500

    Simplified the gate class. User identification process has been integrated in the ticketmachine class

--- src/TicketingSystem/Gate.java
+++ src/TicketingSystem/Gate.java
@@ -1,8 +1,5 @@
 package TicketingSystem;
 
-import java.time.Duration;
-import java.time.Instant;
-
 public class Gate {
 	boolean is_open;
 	
@@ -10,31 +7,15 @@ public class Gate {
 		this.is_open=false;
 	}
 	
-	public void openForCars() {
-		this.is_open = true;
-		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e) {
-			System.out.println("Error: The Gate is broken");
-			System.exit(0);
-		}
-		this.is_open = false;
-	}
-	
-	public void hold() {
+	public void open(){
 		this.is_open = true;
 	}
-	
-	public void cancelHold() {
+
+	public void close(){
 		this.is_open = false;
 	}
 
-	public static void main(String args[]){
-		Instant start = Instant.now();
-		Instant end = Instant.parse("2021-12-05T21:10:00Z");
-		System.out.println(start.toString());
-		System.out.println(end.toString());
-		Duration timeElapsed = Duration.between(start, end);
-		System.out.println("Time taken: "+ timeElapsed.toMillis() / 1000 +" milliseconds");
+	public boolean getStatus(){
+		return this.is_open;
 	}
 }
