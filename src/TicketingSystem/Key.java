3b6fc1995eae5e0bffb5b1859ebe81b7aca286a0
Tianze Shan <tshan@bu.edu>
2021-12-05 18:08:29 -0500

    Update Key.java

--- src/TicketingSystem/Key.java
+++ src/TicketingSystem/Key.java
@@ -1,20 +1,25 @@
 package TicketingSystem;
 
 import java.util.*;
+import ParkingLot.Lot;
+import ParkingLot.Spot;
 
 public class Key {
     String userName;
     boolean active;
+    Spot spot;
 
     public Key(){
         this.userName = "UNKNOWN";
         this.active = false;
+        this.spot = null;
     }
 
-    public Key(String name){
+    public Key(String name, int type, Lot lot){
         this.userName = name;
         Random random = new Random();
         this.active = false;
+        this.spot = lot.assignSpotReg(type, true);
     }
 
     public void activateAccount(){
