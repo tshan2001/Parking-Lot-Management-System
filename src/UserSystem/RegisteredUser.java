1c716ef655f8add4165b949b6fbe10821954ba82
ydai23 <ydai23@bu.edu>
2021-12-06 05:32:44 +0800

    Edited RegisteredUsser and Added one attribute for Vehicle

--- src/UserSystem/RegisteredUser.java
+++ src/UserSystem/RegisteredUser.java
@@ -12,30 +12,106 @@ public class RegisteredUser {
 	boolean disability;
 	Vehicle[] cars;
 	int numCars;
+	Key Key;
+	int credit;
+	String credCard;
+	int leasePeriod;
+
+
+
+	public RegisteredUser() {
+		this.disability = "Unkown";
+		this.username = "None";
+		this.password = "None";
+		this.email = "None";
+		this.unitNum = -1;
+		this.membership = false;
+		this.Key = null;
+		this.cars = new Vehicle[maxicars];
+		this.numCars = 0;
+		this.leasePeriod = 0
+		for (int i=0; i<maxicars;i++) {
+			this.cars[i] = null;
+		}
+		this.creditCard = null;
+		this.credit = 0;
+
 	
-	public RegisteredUser(String usern, String pwd, String email, int unit, boolean disa) {
+	public RegisteredUser(String usern, String pwd, String email, int unit, boolean disa, int leasePeriod p, String creditCard, int credit) {
 		this.disability = disa;
 		this.username = usern;
 		this.password = pwd;
 		this.email = email;
 		this.unitNum = unit;
 		this.membership = false;
+		this.Key = null;
 		this.cars = new Vehicle[maxicars];
 		this.numCars = 0;
+		this.leasePeriod = p
 		for (int i=0; i<maxicars;i++) {
 			this.cars[i] = null;
 		}
+		this.creditCard = creditCard;
+		this.credit = 0;
+
+
+
 	}
 	
-	public void register() {
+	public void memberRegister(Key Key) {
 		this.membership = true;
+		this.Key
 	}
+	puclic Key getMemberKey(){
+		return this.Key;
+		}
 	
 	public void cancel() {
 		this.membership = false;
 	}
-	
-	public void addVehicle(String plate, boolean comp) {
+	public String getAccount(){
+		return this.username;
+	}
+	public boolean isDisable(){
+		return this.disability;
+	}
+	public void setNewPassword(String pwd){
+		this.password = pwd;
+	}
+	public void setNewEmail(String email){
+		this.email = email;
+	}
+	public String getEmail(){
+		return this.email;
+	}
+	public int getUnit(){
+		return this.unit;
+	}
+	public void  moveUit(int unit){
+		this.unit = unit;
+	}
+	public boolean isMember(){
+		return this.membership;
+	}
+	public int getNumCar(){
+		return this.numCars;
+	}
+	public int gerLeasePeriod(){
+		return this.leasePeriod;
+	}
+	public int getCredit(){
+		return this.credit;
+	}
+	public void addCredit(int credit){
+		if this.member == true{
+			println ("you are already a member");
+			return;
+		}
+		this.credit = this.credit + credit;
+		return;
+	}
+
+	public void addVehicle(String plate, boolean comp, char type) {
 		if (this.numCars == maxicars) {
 			System.out.println("You have reached maximum cars registered");
 			return;
@@ -43,10 +119,12 @@ public class RegisteredUser {
 		
 		for (int j=0; j<maxicars; j++) {
 			if (this.cars[j] == null) {
-				this.cars[j] = new Vehicle(plate,comp);
+				this.cars[j] = new Vehicle(plate,comp, type);
 				this.numCars ++;
+				break;
 			}
 		}
+		return;
 	}
 	
 	public void removeVehicle(String plate) {
@@ -59,8 +137,12 @@ public class RegisteredUser {
 			if (this.cars[j].plate == plate) {
 				this.cars[j] = null;
 				this.numCars --;
+				break;
 			}
 		}
+		return;
 	}
+
+
 	
 }
--- src/UserSystem/Vehicle.java
+++ src/UserSystem/Vehicle.java
@@ -1,11 +1,17 @@
 package UserSystem;
 
 public class Vehicle {
-	String plate;
+	String plate; // plate number
 	boolean comp;
+	char type; // what type car it is
+
 	
-	public Vehicle(String plt, boolean comp) {
+	public Vehicle(String plt, boolean comp, int time) {
 		this.plate = plt;
 		this.comp = comp;
+		this.type;
+
 	}
+
+
 }
