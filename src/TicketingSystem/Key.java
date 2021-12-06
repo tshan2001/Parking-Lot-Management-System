7773961c8773d5192b5fd3a66302dc7f0ceb3cec
Ke Li <93945205+like101101@users.noreply.github.com>
2021-12-06 17:20:33 -0500

    Update Key, binds with user and error checking

--- src/TicketingSystem/Key.java
+++ src/TicketingSystem/Key.java
@@ -1,39 +1,55 @@
 package TicketingSystem;
-
-import java.util.*;
 import ParkingLot.Lot;
 import ParkingLot.Spot;
+import UserSystem.RegisteredUser;
 
 public class Key {
-    String userName;
+    RegisteredUser user;
     boolean active;
     Spot spotAssigned;
     int spotType;
 
     public Key(){
-        this.userName = "UNKNOWN";
+    	/* Default constructor for key */
+        this.user = new RegisteredUser();
         this.active = false;
-        this.spotAssigned = null;
+        this.spotAssigned = new Spot();
         this.spotType = -1;
     }
 
-    public Key(String name, int type, Lot lot){
-        this.userName = name;
-        this.active = false;
-        this.spotAssigned = lot.assignSpotReg(type, true);
+    public Key(RegisteredUser user, int type, Lot lot){
+    	/* specified constructor for key */
+        this.user = user;
+        this.active = true;
+        this.spotAssigned = lot.assignSpotReg(type);
         this.spotType = type;
     }
 
     public void activateAccount(){
+    	/* activate the key */
+    	if (this.active) {
+			System.out.println(" ");
+			System.out.println(" <ERROR> : The key is activated right now. ");
+			System.out.println(" ");
+			return;
+    	}
+    	
         this.active = true;
     }
 
     public void deactivateAccount(){
+    	/* deactivate the key */
+    	if (this.active == false) {
+			System.out.println(" ");
+			System.out.println(" <ERROR> : The key is deactivated right now. ");
+			System.out.println(" ");
+			return;
+    	}
         this.active = false;
     }
 
     public String getUserName(){
-        return this.userName;
+        return this.user.getUserName();
     }
 
     public boolean getStatus(){
