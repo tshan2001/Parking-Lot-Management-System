428131ddda8aa7ccb10b538b38ddb24b3845f8e3
Tianze Shan <tshan@bu.edu>
2021-12-05 21:44:31 -0500

    Update Ticket.java

--- src/TicketingSystem/Ticket.java
+++ src/TicketingSystem/Ticket.java
@@ -1,136 +1,43 @@
 package TicketingSystem;
-
-import Database.Database;
 import ParkingLot.Lot;
-import UserSystem.RegisteredUser;
+import ParkingLot.Spot;
 
-import java.time.Duration;
+
+
 import java.time.Instant;
+import java.util.*;
 
-public class TicketMachine {
-    Database database;
-    String machineID;
-    Lot lot;
-    Gate gate;
-    int currentRate;
-    int avaliableReg;
-    int avaliableComp;
-    int avaliableDisa;
+public class Ticket {
+    Instant start;
+    int ID;
+    int pricePer15Min;
+    Spot spot;
 
-    public TicketMachine(){
-        this.machineID = "UNKNOWN";
-        this.currentRate = 0;
-        this.avaliableReg = 0;
-        this.avaliableComp = 0;
-        this.avaliableDisa = 0;
-        this.gate = null;
+    public Ticket() {
+        this.start = Instant.now();
+        Random random = new Random();
+        this.ID = random.nextInt(999999);
+        this.pricePer15Min = 0;
+        this.spot = null;
     }
 
-    public TicketMachine(String name, Lot lot, Database database, Gate gate){
-        this.machineID = name;
-        this.lot = lot;
-        this.database = database;
-        this.currentRate = lot.getPrice();
-        this.avaliableReg = lot.getReg();
-        this.avaliableComp = lot.getComp();
-        this.avaliableDisa = lot.getDisa();
-        this.gate = gate;
+    public Ticket(Lot lot, int type) {
+        this.start = Instant.now();
+        Random random = new Random();
+        this.ID = random.nextInt(999999);
+        this.pricePer15Min = lot.getPrice();
+        this.spot = lot.assignSpotOneTime(type);
     }
 
-    public boolean registeredEntering(Key userKey){
-        if (this.database.userExists(userKey.getUserName())){
-            if (userKey.type == 0){
-                if (this.avaliableReg > 0) {
-                    this.avaliableReg--;
-                }
-                else{
-                    System.out.println("There's no regular parking spot available, please try other types");
-                    return false;
-                }
-            }
-            else if (userKey.type == 1){
-                if (this.avaliableDisa > 0) {
-                    this.avaliableDisa--;
-                }
-                else{
-                    System.out.println("There's no disability parking spot available, please try other types");
-                    return false;
-                }
-            }
-            else if (userKey.type == 2){
-                if (this.avaliableComp > 0) {
-                    this.avaliableComp--;
-                }
-                else{
-                    System.out.println("There's no compact parking spot available, please try other types");
-                    return false;
-                }
-            }
-            return true;
-        }
-        else{
-            System.out.println("User not recogonized, please re-enter username or enter as one time visitor");
-            return false;
-        }
+    public int getPrice(){
+        return this.pricePer15Min;
     }
 
-    public Ticket oneTimeParking(int type) {
-        Ticket ticket = new Ticket(this.lot, type);
-        if (type == 0){
-            if (this.avaliableReg > 0) {
-                this.avaliableReg--;
-                return ticket;
-            }
-            else{
-                System.out.println("There's no regular parking spot available, please try other types");
-            }
-        }
-        else if (type == 1){
-            if (this.avaliableDisa > 0) {
-                this.avaliableDisa--;
-                return ticket;
-            }
-            else{
-                System.out.println("There's no disability parking spot available, please try other types");
-            }
-        }
-        else if (type == 2){
-            if (this.avaliableComp > 0) {
-                this.avaliableComp--;
-                return ticket;                
-            }
-            else{
-                System.out.println("There's no compact parking spot available, please try other types");
-            }
-        }
-        return null;
+    public int getID(){
+        return this.ID;
     }
 
-    public void registeredLeave(Key key, String type){
-        if (this.database.userExists(key.getUserName())){
-            if (type.equals("Reg")){
-                this.avaliableReg++;
-            }
-            else if (type.equals("Disa")){
-                this.avaliableDisa++;
-            }
-            else if (type.equals("Comp")) {
-                this.avaliableComp++;
-            }
-        }
-        else{
-            System.out.println("User not recogonized, please re-enter username or exit the lot by paying your ticket");
-        }
+    public Instant getStartTime(){
+        return this.start;
     }
-
-    public void oneTimeLeave(Ticket ticket){
-        Instant leaveTime = Instant.now();
-        Instant entranceTime = ticket.start;
-        int price = ticket.pricePer15Min;
-        Duration parkingTime = Duration.between(entranceTime, leaveTime);
-        int timeIn15 = (int) parkingTime.toMinutes() / 15;
-        int finalPrice = price * timeIn15;
-        System.out.println("Your total amount due is: " + Integer.toString(finalPrice) + " \nPlease insert your credit card");
-    }
-    
 }
