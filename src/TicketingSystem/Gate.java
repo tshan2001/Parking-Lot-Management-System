76ea07ddece627a9b84f5f4a2d9b21577ec91fd6
Tianze Shan <tshan@bu.edu>
2021-12-05 21:18:20 -0500

    Update Gate.java

--- src/TicketingSystem/Gate.java
+++ src/TicketingSystem/Gate.java
@@ -1,10 +1,16 @@
 package TicketingSystem;
 
 public class Gate {
+	int ID;
 	boolean is_open;
 	
 	public Gate() {
 		this.is_open=false;
+	}
+
+	public Gate(int ID){
+		this.ID = ID;
+		this.is_open = false;
 	}
 	
 	public void open(){
