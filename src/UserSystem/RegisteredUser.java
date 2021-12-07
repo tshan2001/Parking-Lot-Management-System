a54909046bf94a96003965784d8a9fefbbda7391
melodyy0128 <93959403+melodyy0128@users.noreply.github.com>
2021-12-06 22:27:31 -0500

    Update RegisteredUser.java
    deleted unnecessary fields

--- src/UserSystem/RegisteredUser.java
+++ src/UserSystem/RegisteredUser.java
@@ -9,15 +9,9 @@ public class RegisteredUser {
 	String username;
 	String password;
 	String email;
-	int unitNum;
 	boolean membership;
 	boolean disability;
-	Vehicle[] cars;
-	int numCars;
 	Key Key;
-	int credit;
-	String creditCard;
-	int leasePeriod;
 
 
 	public RegisteredUser() {
@@ -25,17 +19,8 @@ public class RegisteredUser {
 		this.username = "do not exist";
 		this.password = "do not exist";
 		this.email = "do not exist";
-		this.unitNum = -1;
 		this.membership = false;
 		this.Key = null;
-		this.cars = new Vehicle[maxicars];
-		this.numCars = 0;
-		this.leasePeriod = 0;
-		for (int i = 0; i < maxicars; i++) {
-			this.cars[i] = null;
-		}
-		this.creditCard = null;
-		this.credit = 0;
 	}
 
 	public RegisteredUser(String usern, String pwd) {
@@ -48,18 +33,8 @@ public class RegisteredUser {
 		this.username = usern;
 		this.password = pwd;
 		this.email = email;
-		this.unitNum = unit;
 		this.membership = false;
 		this.Key = null;
-		this.cars = new Vehicle[maxicars];
-		this.numCars = 0;
-		for (int i=0; i<maxicars;i++) {
-			this.cars[i] = null;
-		}
-		this.credit = 0;
-
-
-
 	}
 
 	public void memberRegister(Key Key) {
@@ -75,86 +50,40 @@ public class RegisteredUser {
 		this.membership = false;
 		this.Key = null;
 	}
+	
 	public String getUserName(){
 		return this.username;
 	}
+	
 	public boolean isDisable(){
 		return this.disability;
 	}
+	
 	public String getPassword (){
 		return this.password;
 	}
+	
 	public void setNewPassword(String pwd){
 		this.password = pwd;
 	}
+	
 	public void setNewEmail(String email){
 		this.email = email;
 	}
+	
 	public String getEmail(){
 		return this.email;
 	}
+	
 	public void changeEmail(String email) {
 		this.email = email;
 		return;
 	}
-	public int getUnit(){
-		return this.unitNum;
-	}
-	public void  moveUit(int unit){
-		this.unitNum = unit;
-	}
+	
 	public boolean isMember(){
 		return this.membership;
 	}
-	public int getNumCar(){
-		return this.numCars;
-	}
-	public int gerLeasePeriod(){
-		return this.leasePeriod;
-	}
-	public int getCredit(){
-		return this.credit;
-	}
-	public void addCredit(int credit){
-		if (this.membership == true){
-			System.out.println ("you are already a member");
-			return;
-		}
-		this.credit = this.credit + credit;
-		return;
-	}
 
-	public void addVehicle(String plate, boolean comp, String type) {
-		if (this.numCars == maxicars) {
-			System.out.println("You have reached maximum cars registered");
-			return;
-		}
-
-		for (int j=0; j<maxicars; j++) {
-			if (this.cars[j] == null) {
-				this.cars[j] = new Vehicle(plate,comp, '0');
-				this.numCars ++;
-				break;
-			}
-		}
-		return;
-	}
-
-	public void removeVehicle(String plate) {
-		if (this.numCars == 0) {
-			System.out.println("You have no car registered");
-			return;
-		}
-
-		for (int j=0; j<maxicars; j++) {
-			if (this.cars[j].plate == plate) {
-				this.cars[j] = null;
-				this.numCars --;
-				break;
-			}
-		}
-		return;
-	}
 
 
 	
