8cb07a3e4f009dfb04f12901c7db3ce035595262
Tianze Shan <tshan@bu.edu>
2021-12-06 20:51:26 -0500

    Updated the ID feature

--- src/TicketingSystem/Ticket.java
+++ src/TicketingSystem/Ticket.java
@@ -1,6 +1,8 @@
 package TicketingSystem;
+import Database.Database;
 import ParkingLot.Lot;
 import ParkingLot.Spot;
+import UserSystem.tempUser;
 
 
 
@@ -8,24 +10,28 @@ import java.time.Instant;
 import java.util.*;
 
 public class Ticket {
+    String ID;
     Instant start;
-    int ID;
     int pricePer15Min;
     Spot spot;
 
     public Ticket() {
+        this.ID = "00000000";
         this.start = Instant.now();
         Random random = new Random();
-        this.ID = random.nextInt(999999);
         this.pricePer15Min = 0;
-        this.spot = null;
+        this.spot = new Spot();
     }
 
     public Ticket(Lot lot, int type) {
-        this.start = Instant.now();
         Random random = new Random();
-        this.ID = random.nextInt(999999);
+        int temp = 0;
+        while (temp < 10000000){
+            temp = random.nextInt(99999999);
+        }
+        this.ID = Integer.toString(temp);
         System.out.println(this.ID);
+        this.start = Instant.now();
         this.pricePer15Min = lot.getPrice();
         this.spot = lot.assignSpotOneTime(type);
     }
@@ -34,11 +40,9 @@ public class Ticket {
         return this.pricePer15Min;
     }
 
-    public int getID(){
-        return this.ID;
-    }
-
     public Instant getStartTime(){
         return this.start;
     }
+
+    public String getID(){ return this.ID; }
 }
