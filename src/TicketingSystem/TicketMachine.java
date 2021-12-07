13b234c69011859a230d453956ab2d6359e00a2c
Ke Li <93945205+like101101@users.noreply.github.com>
2021-12-07 16:51:29 -0500

    Update TicketMachine, now it corrects assign spot

--- src/TicketingSystem/TicketMachine.java
+++ src/TicketingSystem/TicketMachine.java
@@ -27,11 +27,15 @@ public class TicketMachine {
     /* Verify registered user entering the lot */
     public boolean registeredEntering(Key userKey){
         if (this.database.userExists(userKey.getUserName())){
-            System.out.println(userKey.getUserName() + "\nWelcome back!");
+            System.out.println(" ");
+			System.out.println(" - - - - - - - - - - - - - - ");
+            System.out.println("<Parking>: " + userKey.getUserName() + ", Welcome back!");
             return true;
         }
         else{
-            System.out.println("User not recogonized, please re-enter username or enter as one time visitor");
+            System.out.println(" ");
+			System.out.println(" - - - - - - - - - - - - - - ");
+            System.out.println("<Parking>: " + "User not recogonized, please re-enter username or enter as one time visitor");
             return false;
         }
     }
@@ -43,34 +47,45 @@ public class TicketMachine {
         /* regular vehicle */
         if (type == 0){
             if (this.lot.getReg() > 0) {
-                System.out.println("Your ticket ID is: " + ticket.getID() + "\n You will need this ticket or it's ID when you leave");
+                System.out.println(" ");
+			    System.out.println(" - - - - - - - - - - - - - - ");
+                System.out.println("<Parking>: Your ticket ID is: " + ticket.getID());
+                System.out.println("           You will need this ticket or it's ID when you leave");
                 this.database.addTicket(ticket);
                 return ticket;
             }
             else{
-                System.out.println("There's no regular parking spot available, please try other types");
+                System.out.println(" ");
+			    System.out.println(" - - - - - - - - - - - - - - ");
+                System.out.println("<Parking>: There's no regular parking spot available, please try other types");
             }
         }
         /* compact vehicle */
         else if (type == 1){
-            if (this.lot.getDisa() > 0) {
-                System.out.println(ticket.getID());
+            if (this.lot.getComp() > 0) {
+                System.out.println(" ");
+			    System.out.println(" - - - - - - - - - - - - - - ");
+                System.out.println("<Parking>: Your ticket ID is: " + ticket.getID());
+                System.out.println("           You will need this ticket or it's ID when you leave");
                 this.database.addTicket(ticket);
                 return ticket;
             }
             else{
-                System.out.println("There's no disability parking spot available, please try other types");
+                System.out.println("<Parking>: There's no compact parking spot available, please try other types");
             }
         }
         /* disability vehicle */
         else if (type == 2){
-            if (this.lot.getComp() > 0) {
-                System.out.println("Your ticket ID is: " + ticket.getID() + "\n You will need this ticket or it's ID when you leave");
+            if (this.lot.getDisa() > 0) {
+                System.out.println(" ");
+			    System.out.println(" - - - - - - - - - - - - - - ");
+                System.out.println("<Parking>: Your ticket ID is: " + ticket.getID());
+                System.out.println("           You will need this ticket or it's ID when you leave");
                 this.database.addTicket(ticket);
                 return ticket;                
             }
             else{
-                System.out.println("There's no compact parking spot available, please try other types");
+                System.out.println("<Parking>: There's no disability parking spot available, please try other types");
             }
         }
         return new Ticket();
@@ -79,10 +94,12 @@ public class TicketMachine {
     /* Verify registered user leaving the lot */
     public void registeredLeave(Key key){
         if (this.database.userExists(key.getUserName())){
-            System.out.println(key.getUserName() + "\nSafe Travels!");
+            System.out.println(" ");
+			System.out.println(" - - - - - - - - - - - - - - ");
+            System.out.println("<Parking>: "+ key.getUserName() + ", Safe Travels!");
         }
         else{
-            System.out.println("User not recogonized, please re-enter username or exit the lot by paying your ticket");
+            System.out.println("<Parking>: User not recogonized, please re-enter username or exit the lot by paying your ticket");
         }
     }
 
@@ -90,7 +107,7 @@ public class TicketMachine {
     public void oneTimeLeave(String ticketID){
         Instant leaveTime = Instant.now();
         if (!this.database.ticketExists(ticketID)) {
-            System.out.println("Your ticket is invalid, please try again");
+            System.out.println("<Error>: Your ticket is invalid, please try again");
             return;
         }
         Ticket ticket = this.database.getTicket(ticketID);
@@ -101,8 +118,11 @@ public class TicketMachine {
         Duration parkingTime = Duration.between(entranceTime, leaveTime);
         int timeIn15 = (int) parkingTime.toMinutes() / 15;
         int finalPrice = price * timeIn15;
-        System.out.println("Your total amount due is: " + Integer.toString(finalPrice) + " \nPlease insert your credit card");
-        System.out.println("Thank you for parking with us!");
+        System.out.println("<Parking>: Your total amount due is: " + Integer.toString(finalPrice));
+        System.out.println(" ");
+        System.out.println("           Please insert your credit card");
+        System.out.println(" ");
+        System.out.println("           Thank you for parking with us!");
     }
     
 }
